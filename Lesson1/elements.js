979dc49366258989de1be0fb917216c142e2b8da
Glenn Skår <glenn.skar@haugnett.no>
2019-05-03 14:34:51 +0200

    Lesson1 Complete
    Finding HTML elements using Javascript

---
+++ Lesson1/elements.js
@@ -0,0 +1,9 @@
+var class1 = document.getElementsByClassName("class1");
+//alert(class1[0].innerHTML);
+var button1 = document.getElementById("div2")
+//alert(button1.innerHTML);
+var divs =document.getElementsByTagName("DIV");
+
+document.querySelector(".class1");
+var all = document.querySelectorAll(".class1");
+alert(all[1].innerHTML);
